519c56363b71e4fde83fa723254cb514b1f7f88e
wuxinxie <xinxiewu@seas.upenn.edu>
2023-01-07 14:49:35 -0500

    added setup & requirements.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name = 'repo_structure_testing',
+      version ='1.0',
+      # packages=['source', 'source.utils'],
+      url='https://github.com/xinxiewu/repo_structure_testing',
+      # license = 'MIT',
+      author = 'Xinxie Wu',
+      author_email = 'xinxiewu@gmail.com',
+      description = 'This is a sample repository.',
+      install_requires = requirements.txt
+      )
